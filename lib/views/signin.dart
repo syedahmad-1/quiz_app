fe9ca2f0d1874edf00a4a3e9a3734c97a2ed2ca4
syedahmad-1 <sayeadahmad@gmail.com>
2021-12-19 11:30:41 +0530

    signin

--- lib/views/signin.dart
+++ lib/views/signin.dart
@@ -20,20 +20,70 @@ class _SignInState extends State<SignIn> {
         backgroundColor: Colors.black87,
       ),
       body: Form(
+        key: _formkey,
         child: Container(
+          margin: EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             children: [
               Spacer(),
               TextFormField(
-                validator: (val){
-                  return val!.isEmpty? ("Enter Email id"):null;
+                validator: (val) {
+                  return val!.isEmpty ? ("Enter Email id") : null;
                 },
-                decoration: InputDecoration(hintText: "email"),
+                decoration: InputDecoration(hintText: "Email"),
                 onChanged: (val) {
                   email = val;
                 },
               ),
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                validator: (val) {
+                  return val!.isEmpty ? ("Enter Password") : null;
+                },
+                decoration: InputDecoration(hintText: "Password"),
+                onChanged: (val) {
+                  password = val;
+                },
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(vertical: 15),
+                width: MediaQuery.of(context).size.width - 48,
+                child: const Text(
+                  "Sign-in",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                
+                
+              ),
+              SizedBox(
+                height: 18,
+                
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Text("Don't have an account? ", style: TextStyle(fontSize: 16),),
+                  Text("Sign Up", style: TextStyle(fontSize: 16, decoration: TextDecoration.underline),),
+                ],
+              ),
+              SizedBox(height: 80),
             ],
+            
           ),
         ),
       ),
---
+++ lib/views/signup.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SignUP extends StatefulWidget {
+  const SignUP({ Key? key }) : super(key: key);
+
+  @override
+  _SignUPState createState() => _SignUPState();
+}
+
+class _SignUPState extends State<SignUP> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
